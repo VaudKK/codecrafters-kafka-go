1fb52f738aa936b5e4fa93e29ded60e4c7e12a00
VaudKK <vaudkeith@gmail.com>
2025-08-05 00:10:09 +0300

    feat: send correlation id

--- app/main.go
+++ app/main.go
@@ -11,17 +11,22 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Println("Logs from your program will appear here!")
-
 	l, err := net.Listen("tcp", "0.0.0.0:9092")
 	if err != nil {
 		fmt.Println("Failed to bind to port 9092")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+	connection, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	handleConnection(connection)
+}
+
+
+func handleConnection (connection net.Conn){
+	connection.Write([]byte{0,0,0,0,0,0,0,7})
 }
